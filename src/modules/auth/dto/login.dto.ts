92c1043d269c109e6cb9b915dfe43bc4b2964c22
LakshaniNadeesha <2019cs105@stu.ucsc.lk>
2022-10-27 21:56:58 +0530

    Add auth module

---
+++ src/modules/auth/dto/login.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
+export class LoginDto {
+  @IsEmail()
+  @ApiProperty()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  @ApiProperty()
+  password: string;
+} // class
+
+export class SMSVerify {
+  @ApiProperty()
+  phoneNumber: string;
+}
